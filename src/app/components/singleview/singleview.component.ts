8faa5af57503a4acda747b9eda213c7bba29a079
Zomievey <haylee_Hunter@aol.com>
2022-03-26 02:16:34 -0500

    buttons fixed

--- src/app/components/singleview/singleview.component.ts
+++ src/app/components/singleview/singleview.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-single-view',
+  selector: "app-single-view",
   templateUrl: "./singleview.component.html",
   styleUrls: ["./singleview.component.scss"],
 })
- 
 export class SingleViewComponent {
   constructor(private http: HttpClient) {}
   posts: any[] = [];
 
   loadSingle() {
+    const el : any = document.getElementById("show_button");
+    el.style.display = "none";
+
     this.http
       .get(
         "https://youtube.googleapis.com/youtube/v3/playlists?part=snippet,contentDetails&maxResults=10&key=AIzaSyBvfOzusG95vCd7XWMYLTe4Uxd7T_EdEFM&id=PLEx5khR4g7PJELLTYwXZHcimWAwTUaWGA,PLEx5khR4g7PJquVHXtkcdo-QzK54bfmY9,PLEx5khR4g7PIEfXSB9bDS4lB-J9stOynD,PLEx5khR4g7PLCoWS5k9u2WQ8RdKqhKEKn,PLEx5khR4g7PLIOpqqfKfd6OAbzXSa_vBZ"
